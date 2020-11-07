c4114f904b60ba03d40baf881a70f90de34f448c
Michael Lechl <lechl.michael@googlemail.com>
2020-11-07 17:26:07 +0100

    Tested hamcrest matchers.

--- session-01/task-03/WordIndexTest.java
+++ session-01/task-03/WordIndexTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 import java.io.IOException;
 import java.util.TreeMap;
@@ -10,8 +12,8 @@ public class WordIndexTest {
     public void test_word_index_file1() throws IOException {
         TreeMap<String, TreeSet<Integer>> wordIndex = WordIndex.createWordIndex(
                 "/Users/lechlm/Documents/OWN/ps/ps-wise-2021-exercises/session-01/file1.txt");
-        assertEquals(wordIndex.size(), 15);
-        assertTrue(wordIndex.get("Auslesen").contains(1));
+        assertThat(wordIndex.get("Auslesen"), hasItem(1));
+        assertThat(wordIndex.size(), is(15));
     }
 
     @Test
